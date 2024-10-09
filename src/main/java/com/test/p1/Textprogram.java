08d298f1e3451c2c125bedd5e6c18ff596878dfa
hp <hp@LAPTOP-66E293OO>
2024-10-09 12:32:49 +0530

    My first commit

---
+++ src/main/java/com/test/p1/Textprogram.java
@@ -0,0 +1,11 @@
+package com.test.p1;
+
+public class Textprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hy from Eclipse...");
+
+	}
+
+}
